fcbabc37508f24b63c092bdf9efbe5f4eb48281a
steven <steven@peipeiyun.com>
2017-09-21 09:51:30 +0800

    second

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import reducer from './reducers';
+import 'todomvc-app-css/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+	<App />, 
+	document.getElementById('root')
+);
